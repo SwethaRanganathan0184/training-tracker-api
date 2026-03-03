5d776d41041f8982e904f8026ee6d050effd9699
SwethaRanganathan0184 <swetharanganathan0184@gmail.com>
2026-03-03 15:46:05 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite("Data Source=training.db"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddCors(options =>
 {

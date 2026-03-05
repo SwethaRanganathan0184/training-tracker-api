755b987358884e813ae230a216d43ca3caeb2b15
SwethaRanganathan0184 <swetharanganathan0184@gmail.com>
2026-03-05 14:21:22 +0530

    Update Course.cs

--- Models/Course.cs
+++ Models/Course.cs
@@ -1,9 +1,16 @@
+
+// Course.cs
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace TrainingTrackerAPI.Models
 {
     public class Course
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public string CourseName { get; set; }
+        public string CourseName { get; set; } = string.Empty;
         public int Weight { get; set; }
     }
 }

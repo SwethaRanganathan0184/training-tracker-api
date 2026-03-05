ea1a034c0341bdf351b7c6f1f4bcc07789130f0b
SwethaRanganathan0184 <swetharanganathan0184@gmail.com>
2026-03-05 14:15:52 +0530

    Update Employee.cs

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace TrainingTrackerAPI.Models
 {
     public class Employee
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Department { get; set; } = string.Empty;

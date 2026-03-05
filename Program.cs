b0be494a5571d68574ac1ee376abebcf0d47e471
SwethaRanganathan0184 <swetharanganathan0184@gmail.com>
2026-03-05 13:14:54 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(); 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 

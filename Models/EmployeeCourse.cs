f4a478fb9482b9208a501207074f238881047eff
SwethaRanganathan0184 <swetharanganathan0184@gmail.com>
2026-03-05 14:22:03 +0530

    Update EmployeeCourse.cs

--- Models/EmployeeCourse.cs
+++ Models/EmployeeCourse.cs
@@ -1,15 +1,18 @@
+// EmployeeCourse.cs
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace TrainingTrackerAPI.Models
 {
     public class EmployeeCourse
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-
         public int EmployeeId { get; set; }
-        public Employee Employee { get; set; }   // navigation
-
+        public Employee Employee { get; set; } = null!;
         public int CourseId { get; set; }
-        public Course Course { get; set; }       // navigation
-
+        public Course Course { get; set; } = null!;
         public bool IsCompleted { get; set; }
     }
 }
